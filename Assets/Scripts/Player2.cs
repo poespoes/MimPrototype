c23167ed029d8a0571e82bf9c97fe5628521bde2
Unknown <poe_s@hotmail.com>
2018-08-05 17:54:22 -0400

    Exercise 3 update

---
+++ Assets/Scripts/Player2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2 : MonoBehaviour {
+    public KeyCode rightKey = KeyCode.RightArrow;
+    public KeyCode leftKey = KeyCode.LeftArrow;
+    public KeyCode upKey = KeyCode.UpArrow;
+    public KeyCode downKey = KeyCode.DownArrow;
+
+    public float moveSpeed;
+    public float jumpSpeed;
+
+    private Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(rightKey)) {
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+            Debug.Log("moving");
+        }
+    }
+}
---
+++ Assets/Scripts/RotateIcon.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+namespace DigitalRubyShared {
+
+    public class RotateIcon : MonoBehaviour {
+        public GameObject RotateArea;
+        public GameObject ScaleArea;
+        public GameObject Leaf;
+
+        private OneTouchRotateGestureRecognizer rotationGesture = new OneTouchRotateGestureRecognizer();
+        private OneTouchScaleGestureRecognizer scaleGesture = new OneTouchScaleGestureRecognizer();
+
+        private bool GestureIntersectsSprite(DigitalRubyShared.GestureRecognizer g, GameObject obj) {
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(new Vector3(g.StartFocusX, g.StartFocusY, -Camera.main.transform.position.z));
+            Collider2D col = Physics2D.OverlapPoint(worldPos);
+            return (col != null && col.gameObject != null && col.gameObject == obj);
+        }
+
+        private void RotationGestureUpdated(DigitalRubyShared.GestureRecognizer gesture) {
+            if (gesture.State == GestureRecognizerState.Began && !GestureIntersectsSprite(gesture, RotateArea)) {
+                gesture.Reset();
+            } else if (gesture.State == GestureRecognizerState.Executing) {
+                Leaf.transform.Rotate(0.0f, 0.0f, rotationGesture.RotationDegreesDelta);
+            }
+        }
+
+        private void ScaleGestureUpdated(DigitalRubyShared.GestureRecognizer gesture) {
+            if (gesture.State == GestureRecognizerState.Began && !GestureIntersectsSprite(gesture, ScaleArea)) {
+                gesture.Reset();
+            } else if (gesture.State == GestureRecognizerState.Executing) {
+                Leaf.transform.localScale *= scaleGesture.ScaleMultiplier;
+            }
+        }
+
+        private void Start() {
+            FingersScript.Instance.AddGesture(rotationGesture);
+            rotationGesture.StateUpdated += RotationGestureUpdated;
+
+            FingersScript.Instance.AddGesture(scaleGesture);
+            scaleGesture.StateUpdated += ScaleGestureUpdated;
+
+            // if you wanted to rotate the earth from the center of the earth rather than the button, you could do this:
+            //Vector3 screenPos = Camera.main.WorldToScreenPoint(Earth.transform.position);
+            //rotationGesture.AnglePointOverrideX = screenPos.x;
+            //rotationGesture.AnglePointOverrideY = screenPos.y;
+        }
+
+        private void Update() {
+
+        }
+    }
+}
