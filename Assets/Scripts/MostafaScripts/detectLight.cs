379f855aa7f0655398fc9019e60b048e14ba42a7
Mostafa Haque <93haque@gmail.com>
2018-08-19 13:59:31 -0400

    new beam implemented

---
+++ Assets/Scripts/MostafaScripts/detectLight.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class detectLight : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D col) {
+        Debug.Log("OnCollisionEnter2D");
+
+        if (col.gameObject.name == "beam") {
+
+        }
+    }
+}
