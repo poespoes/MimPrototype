6529bd16d6a385c1ffd831b5d64a75af838ce895
Unknown <poe_s@hotmail.com>
2018-09-02 16:45:31 -0400

    Thesis jam update

---
+++ Assets/BeamController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeamController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 mousePos = GetWorldPositionOnPlane(Input.mousePosition, transform.position.z);
+        mousePos -= transform.position;
+        Vector3 euler = transform.eulerAngles;
+        euler.z = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        transform.eulerAngles = euler;
+	}
+
+    public Vector3 GetWorldPositionOnPlane(Vector3 screenPosition, float z) {
+        Ray ray = Camera.main.ScreenPointToRay(screenPosition);
+        Plane xy = new Plane(Vector3.forward, new Vector3(0, 0, z));
+        float distance;
+        xy.Raycast(ray, out distance);
+        return ray.GetPoint(distance);
+    }
+}
---
+++ Assets/Monster.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour {
+
+    public List<Sprite> spriteList;
+    public float frameDuration;
+    public float frameReverseDuration;
+    bool isLightened = false;
+    float timer = 0;
+    int currentIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (isLightened == false) {
+            if (timer >= frameDuration) {
+                timer -= frameDuration;
+                if (currentIndex + 1 < spriteList.Count) {
+                    currentIndex++;
+                }
+                GetComponent<SpriteRenderer>().sprite = spriteList[currentIndex];
+            }
+        } else {
+            if (timer >= frameReverseDuration) {
+                timer -= frameReverseDuration;
+                if (currentIndex - 1 >= 0) {
+                    currentIndex--;
+                }
+                GetComponent<SpriteRenderer>().sprite = spriteList[currentIndex];
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        isLightened = true;
+        timer = 0;
+    }
+
+    void OnTriggerExit(Collider other) {
+        isLightened = false;
+        timer = 0;
+    }
+}
